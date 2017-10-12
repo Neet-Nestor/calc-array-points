b818692f095687c70573ff0596e492ae413ae91f
Student User <studentuser@D-10-18-223-145.dhcp4.washington.edu>
2017-10-12 14:31:33 -0700

    Ver 1.0

--- Calc Array Points/main.swift
+++ Calc Array Points/main.swift
@@ -56,5 +56,45 @@ func avg(nums: Int...) -> Double {
     return Double(sum) / Double(nums.count)
 }
 
+func addPoints(point_1 p1:(x:Int, y:Int), point_2 p2:(x:Int, y:Int)) -> (x:Int, y:Int) {
+    let xSum:Int = p1.x + p2.x
+    let ySum:Int = p1.y + p2.y
+    return (xSum, ySum)
+}
+
+func subtractPoints(point_1 p1:(x:Int, y:Int), point_2 p2:(x:Int, y:Int)) -> (x:Int, y:Int) {
+    let xSum:Int = p1.x - p2.x
+    let ySum:Int = p1.y - p2.y
+    return (xSum, ySum)
+}
+
+func addPoints(point_1 p1:[String: Int], point_2 p2:[String: Int]) -> [String: Int]{
+    if (p1["x"] != nil && p1["y"] != nil &&
+        p2["x"] != nil && p2["y"] != nil) {
+        let xSum: Int = p1["x"]! + p2["x"]!
+        let ySum: Int = p1["y"]! + p2["y"]!
+        return ["x": xSum, "y": ySum]
+    } else {
+        print("Error")
+        return ["Error": -1]
+    }
+}
+
+func subtractPoints(point_1 p1:[String: Int], point_2 p2:[String: Int]) -> [String: Int]{
+    if (p1["x"] != nil && p1["y"] != nil &&
+        p2["x"] != nil && p2["y"] != nil) {
+        let xSum: Int = p1["x"]! - p2["x"]!
+        let ySum: Int = p1["y"]! - p2["y"]!
+        return ["x": xSum, "y": ySum]
+    } else {
+        print("Error")
+        return ["Error": -1]
+    }
+}
+
+
+
 print(calculate(operation: add, num_1: 15, num_2: 5))
 print(add(nums: 12, 23, 42, 21, 42))
+print(addPoints(point_1: (x: 3, y: 5), point_2: (x: 5, y: -2)))
+print(addPoints(point_1: ["z": 5.0, "y": 6], point_2: ["e" : 10, "y": -2]))
